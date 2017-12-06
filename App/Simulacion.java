b36f728706190a0270acf4fedf79a99bd428aad1
Alejandro Lobato <alejandrolm@outlook.com>
2017-12-06 11:42:53 -0600

    Just saved some files

---
+++ App/Simulacion.java
@@ -0,0 +1,25 @@
+/*
+ *
+ *                Applicacion Ponchito
+ *
+ * Authors:         ID: 153542      Luis Alberto Flores
+ *                  ID: 154422      Alejandro Lobato
+ *                  ID: 153424      Jose Antonio Marquez
+ *
+ * Date:            Dec/08/2017
+ *
+ */
+
+
+
+/*
+ *				Simulacion Class
+ *
+ * Esta clase permite realizar las simulaciones de los viajes.
+ *
+ */
+
+
+public class Simulacion {
+
+}
